96a3333065eaca5f0c705327bb67bd278ce0c2e0
0748926625 <nanou.guymartial@gmail.com>
2025-06-15 17:18:46 +0000

    Create MainActivity.kt

---
+++ app/src/main/java/com/klata/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.klata.app
+
+import android.os.Bundle
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val webView = WebView(this)
+        setContentView(webView)
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("file:///android_asset/www/index.html")
+    }
+}
